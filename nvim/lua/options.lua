558ac8e40fa517a896b04d1485aba983868a811f
Celio Reyes <celio@flyclops.com>
2024-11-27 12:37:40 -0500

    feat: adding neovide mac support

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,12 +1,20 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
-
+require("nvchad.options")
+-- add yours here!
 local o = vim.o
 -- Indenting
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
 
--- o.cursorlineopt ='both' -- to enable cursorline!
+local g = vim.g
+if g.neovide then
+    g.neovide_input_macos_option_key_is_meta = "only_left"
+    g.neovide_cursor_trail_size = 0
+    g.neovide_transparency = 0.9
+end
 
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
